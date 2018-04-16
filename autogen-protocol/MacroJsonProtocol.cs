4017cba963a20168d7ffcad84527ed22f85f84dd
badlabor <badlabor@work.com>
2018-04-16 11:39:56 +0800

    自动生成代码

---
+++ autogen-protocol/MacroJsonProtocol.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class PBasePlayerInfo
+{
+    public Int64 PlayerId;
+    public int HeadId;
+    public int Level;
+    public int Vip;
+    public string PlayerName;
+    public int Fight;
+}
+public class PFamWarSimpleInfoOne
+{
+    public int CityId;
+    public Int64 WinnerFamily;
+    public string WinnerFamilyName;
+    public Int64 ChairmanPlayerId;
+    public string ChairmanName;
+    public int ChairmanHead;
+    public int ChairmanLevel;
+    public int AttackFamilyCnt;
+    public Int64 LastSetChairmanTime;
+}
+public class PFamWarSimpleInfo
+{
+    public List<PFamWarSimpleInfoOne> AllInfo;
+    public List<int> MyCities;
+    public int MyAuth;
+}
+public class PFamWarDetailedInfo
+{
+    public PFamWarSimpleInfoOne CityInfo;
+    public Int64 MyFamilyId;
+    public List<PFamWarTeamInfo> AllTeamInfo;
+}
+public class PFamWarTeamInfo_Player
+{
+    public Int64 PlayerId;
+    public int HeadId;
+    public string PlayerName;
+}
+public class PFamWarTeamInfo
+{
+    public Int64 FamilyId;
+    public string FamilyName;
+    public Int64 FightAbility;
+    public int Liveness;    // 活跃值
+    public List<PFamWarTeamInfo_Player> Players;
+}
+public class PFamWarCityId
+{
+    public int cityid;
+}
+public class PMyUsedCard
+{
+    public List<int> UsedCards;
+    public int CityId;
+    public List<int> CityCards;
+}
+public class PAllMyUsedCard
+{
+    public List<PMyUsedCard> AllCityCards;
+}
+
+public class PSimpleGameCard
+{
+    public int card_sid_;
+    public int card_star_;
+    public int card_rank_;
+    public int card_level;
+}
+public class PSimpleGameCardWithHp
+{
+    public PSimpleGameCard Card;
+    public int hp_percent;
+}
+public class PFamWarLineup
+{
+    public PFamWarTeamInfo_Player player;
+    public int FightAbility;
+    public List<PSimpleGameCard> cards;
+}
+public class PFamWarOneBattle
+{
+    public int guid;
+    public PFamBattleTeamInfo Team0;
+    public PFamBattleTeamInfo Team1;
+    public int WinnerIdx;
+    public string RecordGUID;
+}
+public class PFamWarMeritPoint
+{
+    public int HeadId;
+    public string PlayerName;
+    public int Point;
+}
+public class PFamWarFamilyBattle
+{
+    public int CityId;
+    public int BattleIdx;
+    public int BattleMaxCnt;
+    public List<PFamWarOneBattle> AllBattles;
+    public List<PFamWarMeritPoint> Team0;
+    public List<PFamWarMeritPoint> Team1;
+    public int FamilyFightAbility0;
+    public string FamilyName0;
+    public int FamilyFightAbility1;
+    public string FamilyName1;
+    public int WinnerIdx;	// 胜利方：team0，还是team1
+}
+public class PFamWarCityBattleSimpleInfo
+{
+    public int CityId;
+    public int BattleTotalCount;
+}
+public class PFamWarCityBattleSimpleInfoList
+{
+    public List<PFamWarCityBattleSimpleInfo> AllCities;
+}
+public class PFamBattleTeamInfo
+{
+    public PFamWarTeamInfo_Player Player;
+    public int PlayerLevel;
+    public int FightAblitity;
+    public List<PSimpleGameCardWithHp> Cards;
+}
+public class PFamWarMeritPointDetail
+{
+    public PBasePlayerInfo Base;
+    public int OldMerit;
+    public int AllMerit;
+}
+public class PFamWarMeritPointDetailList
+{
+    public List<PFamWarMeritPointDetail> All;
+    public Int64 LastUnSetChairmanTime;
+}
+public class PBroadcastFamWarStage
+{
+    public int StateId;
+    public Int64 NextStageTime;
+    public Int64 CurStageBeginTime;
+    public int MyAuth;
+}
+public class PFamRewardInfo
+{
+    public int cityid;
+    public int reward;
+    public int extra;
+    public int got;
+};
+
+public class PFamMyCity
+{
+    public int cityid;
+    public int status;
+}
+public class PFamMyCityList
+{
+    public List<PFamMyCity> AllCities;
+}
+public class PFamWarInspireMsg
+{
+    public Int64 FamilyId;
+    public string PlayerName;
+    public int InspireType;
+}
+public class PFamWarInspireMsgAll
+{
+    public int CityId;
+    public List<PFamWarInspireMsg> AllMsg;
+}
+public class PFamWarInspireInfo
+{
+    public List<int> FourInpire;
+    public PFamWarInspireMsgAll AllMsg;
+}
+
+public class PFamMeritPoint
+{
+    public int Point;
+};
+
+public class PFamDeclareWarRet
+{
+    public int ErrorCode;
+}
+public class PFamBroadcastChairman
+{
+    public int CityId;
+    public Int64 ChairmanPlayerId;
+    public string ChairmanName;
+    public int ChairmanHead;
+    public int ChairmanLevel;
+    public Int64 LastSetChairmanTime;
+}
---
+++ autogen-protocol/auto.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Reflection;
+using System.Globalization;
+using Microsoft.CSharp;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Text;
+using System.Collections.Generic;
+using System.IO;
+using NVelocity;
+using NVelocity.App;
+using NVelocity.Runtime;
+
+namespace autogen_protocol
+{
+    class ClassInfo
+    {
+        public string Name { get; set; }
+        public List<FieldInfo> Fields { get; set; }
+    };
+
+    class TypeUtil
+    {
+        public string TypeString(FieldInfo f)
+        {
+            if (f.FieldType.GetInterface(typeof(System.Collections.IList).Name) != null)
+            {
+                return string.Format("std::vector<{0}>", dictName(f.FieldType.GenericTypeArguments[0].Name));
+            }
+
+            return dictName(f.FieldType.Name);
+        }
+        public string ConstructInfo(ClassInfo cls)
+        {
+            StringBuilder buffer = new StringBuilder();
+            buffer.Append(cls.Name);
+            buffer.Append("()");
+
+            int cnt = 0;
+            foreach (var f in cls.Fields)
+            {
+                string v = "";
+                if (f.FieldType == typeof(int) || f.FieldType == typeof(Int64))
+                {
+                    v = string.Format("{0}({1})", f.Name, "0");
+                }
+                else if (f.FieldType == typeof(bool))
+                {
+                    v = string.Format("{0}({1})", f.Name, "false");
+                }
+
+                if (!string.IsNullOrEmpty(v))
+                {
+                    if (cnt == 0)
+                    {
+                        buffer.Append(":");
+                    }
+                    if (cnt > 0)
+                    {
+                        buffer.Append(",");
+                    }
+                    if (cnt%3 == 2)
+                    {
+                        buffer.Append("\r\n\t\t");
+                    }
+                    buffer.Append(v);
+                    cnt++;
+                }
+            }
+            buffer.Append("\r\n\t{}");
+
+            return buffer.ToString();
+        }
+
+        string dictName(string s)
+        {
+            if (dict.ContainsKey(s))
+            {
+                return dict[s];
+            }
+            throw new Exception("类型必须提前定义：" + s);
+        }
+        public Dictionary<string, string> dict;
+    }
+
+
+    class auto
+    {
+        // 加载".cs"文件
+        public static Assembly CompileFiles(string[] files)
+        {
+            // 1.CSharpCodePrivoder
+            CSharpCodeProvider objCSharpCodePrivoder = new CSharpCodeProvider();
+            // 2.ICodeComplier
+            ICodeCompiler objICodeCompiler = objCSharpCodePrivoder.CreateCompiler();
+            // 3.CompilerParameters
+            CompilerParameters objCompilerParameters = new CompilerParameters();
+            objCompilerParameters.ReferencedAssemblies.Add("System.dll");
+            objCompilerParameters.GenerateExecutable = false;
+            objCompilerParameters.GenerateInMemory = true;
+            // 4.CompilerResults
+            CompilerResults cr = objICodeCompiler.CompileAssemblyFromFileBatch(objCompilerParameters, files);
+            if (cr.Errors.HasErrors)
+            {
+                Console.WriteLine("编译错误：");
+                foreach (CompilerError err in cr.Errors)
+                {
+                    Console.WriteLine(err.ErrorText);
+                }
+                return null;
+            }
+            else
+            {
+                // 通过反射，调用HelloWorld的实例
+                //Assembly objAssembly = cr.CompiledAssembly;
+                //object objHelloWorld = objAssembly.CreateInstance("DynamicCodeGenerate.HelloWorld");
+                //MethodInfo objMI = objHelloWorld.GetType().GetMethod("OutPut");
+                //Console.WriteLine(objMI.Invoke(objHelloWorld, null));
+                Console.WriteLine("编译成功！");
+                return cr.CompiledAssembly;
+            }
+        }
+        public static void ExecAll()
+        {
+            string[] files = { "MacroJsonProtocol.cs" };
+            var assemble = CompileFiles(files);
+            if (assemble == null)
+            {
+                return;
+            }
+
+            var types = assemble.GetTypes();
+            List<ClassInfo> AllCls = new List<ClassInfo>();
+            foreach (var t in types)
+            {
+                // 取出所有public成员变量。
+                var fileds = t.GetFields();
+
+                ClassInfo cls = new ClassInfo();
+                cls.Name = t.Name;
+                cls.Fields = new List<FieldInfo>();
+                foreach (var f in fileds)
+                {
+                    cls.Fields.Add(f);
+                }
+                AllCls.Add(cls);
+            }
+
+            // 生成c++代码
+            foreach (var cls in AllCls)
+            {
+                
+            }
+            GenerateCode(AllCls);
+        }
+        public static void GenerateCode(List<ClassInfo> AllCls)
+        {
+            var util = new TypeUtil();
+            util.dict = new Dictionary<string, string>();
+
+            util.dict.Add(typeof(int).Name, "int");
+            util.dict.Add(typeof(Int64).Name, "Int64");
+            util.dict.Add(typeof(string).Name, "std::string");
+
+            foreach (var cls in AllCls)
+            {
+                util.dict.Add(cls.Name, cls.Name);
+            }
+
+
+            VelocityEngine ve = new VelocityEngine();
+            ve.Init();
+
+
+            VelocityContext vc = new VelocityContext();
+            //传入模板所需要的参数
+            vc.Put("AllCls", AllCls); //设置参数为对象，在模板中可以通过$dic.dudu 来引用
+            vc.Put("util", util);
+
+
+            StringWriter sw = new StringWriter();
+            // 注意cls.Name必须是属性，而不是普通的成员变量，要不会无效！
+            string vt = File.ReadAllText("../../template.txt");
+            ve.Evaluate(vc, sw, "eval1", vt);
+            
+            Console.WriteLine(sw.GetStringBuilder().ToString());
+
+            File.WriteAllText("../../auto.h", sw.GetStringBuilder().ToString());
+        }
+    }
+}
